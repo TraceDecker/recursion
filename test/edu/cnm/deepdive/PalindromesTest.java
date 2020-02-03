6073d85499be2c490005a5773e30ffb3bfb6819b
TraceDecker <decker1041@gmail.com>
2020-02-03 11:24:20 -0700

    added unit test for affirmative and negative.

---
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -0,0 +1,38 @@
+package edu.cnm.deepdive;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PalindromesTest {
+
+  static final String[] positiveTestData = {
+      "racecar",
+      "Race Car",
+      "radar",
+      "",
+      "x",
+      "Xx",
+      "A man, a plan, a canal - Panama!"
+  };
+
+  static final String[] negativeTestData = {
+      "xY",
+      "sonar",
+      "Madame, I'm Adam",
+      "burrito dog"
+  };
+
+  @Test
+  void isPalindromeAffirmative() {
+    for (String testCase : positiveTestData) {
+      assertTrue(Palindromes.isPalindrome(testCase));
+    }
+  }
+  @Test
+  void isPalindromeNegative() {
+    for (String testCase : negativeTestData) {
+      assertFalse(Palindromes.isPalindrome(testCase));
+    }
+  }
+}
